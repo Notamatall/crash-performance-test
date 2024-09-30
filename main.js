851731494d3b121f5dbb7186b3448ac19a3c0e2f
Yaroslav.Hyrskiy <yaroslav.hyrskiy>
2024-09-30 20:13:18 +0300

    socket added

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 require("dotenv").config({ path: "./.env" });
 const { getJson } = require("./utils"); // './math' is the path to the file
 const axios = require("axios");
+const io = require("socket.io-client");
 
 const usersCookies = new Map();
 const users = [];
@@ -28,8 +29,6 @@ async function authenticate(user) {
   }
 }
 
-async function getUserInfo(user) {}
-
 async function fetchUserData(user) {
   const url = `${process.env.BACKEND_URL}${process.env.ME_ENDPOINT}`;
   const cookies = usersCookies.get(user.id);
@@ -41,6 +40,33 @@ async function fetchUserData(user) {
 
   console.log(response.data);
 }
+
+async function createUserSocketConnection(user) {
+  const url = `${process.env.BACKEND_URL}${process.env.ME_ENDPOINT}`;
+  const socket = io(url, {
+    withCredentials: true,
+    transports: ["websocket"],
+  });
+
+  socket.on;
+}
+
+function connectSocket(accessToken) {
+  return new Promise((resolve, reject) => {
+    const socket = io("http://localhost:3000", {
+      withCredentials: true,
+      transports: ["websocket"],
+    });
+
+    socket.on("connect", () => {
+      resolve(socket);
+    });
+
+    socket.on("connect_error", (error) => {
+      reject(error);
+    });
+  });
+}
 async function main() {
   try {
     const users = await getJson("users.json");
@@ -50,5 +76,11 @@ async function main() {
     console.error(err);
   }
 }
+const socket = io("http://localhost:3000", {
+  query: {
+    token: accessToken,
+  },
+  transports: ["websocket"],
+});
 
 main();
